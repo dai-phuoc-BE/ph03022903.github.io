749c2870489b34fb517cbab334f7b894e28f7a47
ph03022903 <ph03022903@users.noreply.github.com>
2022-03-04 10:39:50 +0700

    add

--- js/home.js
+++ js/home.js
@@ -7,31 +7,12 @@ TweenMax.staggerTo($(".box-center"), 5, { opacity: 1 }, 0.1);
 //   $(".box-card").delay(1000).hide(1000);
 //   TweenMax.staggerTo($(".box-center-2"), 5, { opacity: 1 }, 0.1);
 // });
-TweenMax.set($(".box-center-2"), { opacity: 0 });
-
-// $(".box").on("mouseenter", function () {
-//   var duration = 1;
-//   TweenMax.to(this, duration / 4, { y: -50, ease: Power2.easeOut });
-//   TweenMax.to(this, duration / 2, {
-//     y: 0,
-//     ease: Bounce.easeOut,
-//     delay: duration / 4,
-//   });
-// });
-
-// var bounce_obj = $(".bounce");
-// var duration = 1;
-// var tl = new TimelineMax({ repeat: -1, repeatDelay: 1 });
-
-// tl.set(bounce_obj, { y: 0 })
-//   .to(bounce_obj, duration / 4, { y: +20, ease: Power2.easeOut }, "bounceme")
-//   .to(
-//     bounce_obj,
-//     duration / 2,
-//     { y: 0, ease: Bounce.easeOut, delay: duration / 4 },
-//     "bounceme"
-//   );
-
+$(".button-box-music").click(function () {
+  TweenMax.staggerTo($(".box-card"), 1, { y: -700 }, 0);
+  $(".box-card").delay(1000).hide(1000);
+  TweenMax.staggerTo($(".box-music"), 5, { opacity: 1 }, 0.1);
+});
+TweenMax.set($(".box-music"), { opacity: 0 });
 document.addEventListener(
 	"DOMContentLoaded",
 	function () {
